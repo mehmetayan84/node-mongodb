daa1b1a323e35110057b778ca0138b83f5a0bca7
MehmetAyan <mehmetayan84@gmail.com>
2018-04-07 11:21:54 +0300

    Some enchancements

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://root:root4node@ds231589.mlab.com:31589/node-todo');
 
 module.exports = {
   mongoose: mongoose

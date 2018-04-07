aa1111ddcdc0b4560c5eeaf9c05527ea72c66368
MehmetAyan <mehmetayan84@gmail.com>
2018-04-07 11:17:31 +0300

    Setup seperate test and development env

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env == 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://root:root4node@ds231589.mlab.com:31589/node-todo'
+} else if (env == 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://root:root4node@ds237379.mlab.com:37379/node-todo-test';
+}

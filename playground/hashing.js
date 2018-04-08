a8eb0a54e97360aaf63d0525e5c7e2f5a0ba8685
MehmetAyan <mehmetayan84@gmail.com>
2018-04-09 00:03:19 +0300

    password hashing is added

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-console.log(JSON.stringify(data));
+// bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//    });
+// });
 
-var token = jwt.sign(data, 'mehmet');
+var hashed = '$2a$10$PTpsJD45Ag2nhXY01k/.2elcCtdRd.3BES0zKyc7JEC7rGTDUGr/u';
 
-console.log(token);
+bcrypt.compare(password, hashed, (err, res) => {
+   console.log(res);
+});
 
-var decode = jwt.verify(token, 'mehmet');
+// var data = {
+//     id: 10
+// };
 
-console.log(decode);
+// console.log(JSON.stringify(data));
+//
+// var token = jwt.sign(data, 'mehmet');
+//
+// console.log(token);
+//
+// var decode = jwt.verify(token, 'mehmet');
+//
+// console.log(decode);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();

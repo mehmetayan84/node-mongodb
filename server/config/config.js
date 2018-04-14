a36beeb8d4d055b4c54f6f1f44ca28f8fe20fee2
MehmetAyan <mehmetayan84@gmail.com>
2018-04-14 22:45:42 +0300

    Update to use config.json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env == 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://root:root4node@ds231589.mlab.com:31589/node-todo';
-} else if (env == 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://root:root4node@ds237379.mlab.com:37379/node-todo-test';
+if(env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+       process.env[key] = envConfig[key];
+    });
 }
